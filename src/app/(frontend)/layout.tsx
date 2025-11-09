9a49d864fc379e38f717279f677df16e132413da
Olek-B <u57_alebla_waw@technischools.com>
2025-11-09 19:45:39 +0100

    feat: added stuff

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -36,6 +36,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
 
           <Header />
           {children}
+          <h1>stuff</h1>
           <Footer />
         </Providers>
       </body>
